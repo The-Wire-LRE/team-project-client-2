4277d1ab5416ccc3b7946dbdddaa5345b07562a5
eabrigham <eabrigham@gmail.com>
2018-08-28 19:13:22 -0400

    Added buttons and modals in HTML and click handlers in app.js document ready
    Authentication currently working.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const authEvents = require('./auth/events.js')
+const postEvents = require('./post/events.js')
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +10,6 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  authEvents.addHandler()
+  postEvents.addHandler()
 })
